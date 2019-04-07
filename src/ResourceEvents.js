81577ff605d93e9b78ae9d3fbd9eb4d95316bd06
Victor Cristinari <victor@buildhero.ai>
2019-04-08 00:31:06 +0300

    overlaps

--- src/ResourceEvents.js
+++ src/ResourceEvents.js
@@ -211,7 +211,7 @@ class ResourceEvents extends Component {
                         let isEnd = eventEnd <= durationEnd;
                         let left = index*cellWidth + (index > 0 ? 2 : 3);
                         let width = (evt.span * cellWidth - (index > 0 ? 5 : 6)) > 0 ? (evt.span * cellWidth - (index > 0 ? 5 : 6)) : 0;
-                        let top = marginTop + idx*config.eventItemLineHeight;
+                        let top = marginTop; // + idx*config.eventItemLineHeight;
                         let eventItem = <DnDEventItem
                                                    {...this.props}
                                                    key={evt.eventItem.id}
